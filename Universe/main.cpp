201975cda6709b40fe3e7f4ceaeb4c37d1cae5f7
Aquilla Sherrock <acsherrock@Aquillas-MacBook-Pro.local>
2014-11-17 18:59:24 -0600

    Initial Commit

---
+++ Universe/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Universe
+//
+//  Created by Aquilla Sherrock on 11/17/14.
+//  Copyright (c) 2014 Aquilla Sherrock. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
